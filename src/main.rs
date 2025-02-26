f0b4ab188c409911920634d806e6f63812570f0b
Kenedy Nopriansyah <kenedynopriansyah@192.168.1.18>
2025-02-26 15:09:25 +0700

    if else statement

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
-    println!("Hai Kenedy Nopriansyah");
+    let num = 32;
+
+    if num > 34 {
+        println!("Nilai ini lebih besar dari Nilai {}", num);
+    } else if num >= 32 {
+        println!("Nilai ini sama besar dengan Nilai {}", num);
+    } else {
+        println!("Nilai ini lebih kecil dari Nilai {}", num);
+    }
 }
