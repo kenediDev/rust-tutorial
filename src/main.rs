6109e836c51e4d97acd4a59f4fd447c27a28f6c8
Kenedy Nopriansyah <kenedynopriansyah@192.168.1.18>
2025-02-26 18:33:15 +0700

    tuples

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
-    println!("Hai Kenedy Nopriansyah");
+    let mut tuples = (10, 1.30, "Kenedy Nopriansyah", (90, 9.20, "Kenedy"));
+
+    println!("{:?}", tuples);
+    println!("{:?}", (tuples.3).2);
+
+    (tuples.3).2 = "Channel youtube Kenedy Nopriansyah";
+
+    println!("{}", (tuples).2);
+    println!("{:?}", tuples)
 }
