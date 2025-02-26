9c604b6a0c561f9e2a493ce86576419b7897b636
Kenedy Nopriansyah <kenedynopriansyah@Kenedys-MacBook-Air.local>
2025-02-26 10:39:51 +0700

    how to use comment in rust programming

---
+++ src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    // ini adalah komen didalam bahasa rust programming
+
+    /*
+       nama      : Kenedy Nopriansyah
+       deskripsi : Sedang belajar bahasa rust programming
+    */
+
+    print!("Hai Nama Saya Adalah ");
+    println!("Kenedy Nopriansyah jangan lupa like and subscribe :)");
+    print!("Hello, world!");
+}
