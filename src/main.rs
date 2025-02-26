21592be98cca9f9b3d3d5de76daf79d8f40d2b3a
Kenedy Nopriansyah <kenedynopriansyah@192.168.1.18>
2025-02-26 18:41:46 +0700

    enums

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+enum Direction {
+    Up,
+    Down,
+    Left,
+    Right,
+}
+
 fn main() {
-    println!("Hai Kenedy Nopriansyah");
+    let direction: Direction = Direction::Down;
+
+    match direction {
+        Direction::Up => println!("ini dari direction up"),
+        Direction::Down => println!("ini dari direction down"),
+        Direction::Left => println!("ini dari direction left"),
+        Direction::Right => println!("ini dari direction right"),
+    }
 }
