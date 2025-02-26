93d6ee6a62375210ac6c61b71f6e3156a6057b2f
Kenedy Nopriansyah <kenedynopriansyah@192.168.1.18>
2025-02-26 11:13:27 +0700

    modifier main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,3 @@
 fn main() {
-    // ini adalah komen didalam bahasa rust programming
-
-    /*
-       nama      : Kenedy Nopriansyah
-       deskripsi : Sedang belajar bahasa rust programming
-    */
-
-    print!("Hai Nama Saya Adalah ");
-    println!("Kenedy Nopriansyah jangan lupa like and subscribe :)");
-    print!("Hello, world!");
+    println!("Hai Kenedy Nopriansyah");
 }
