26045f10eb75b788a074a2dff558998513c5bdc0
Kenedy Nopriansyah <kenedynopriansyah@192.168.1.18>
2025-02-26 11:46:41 +0700

    data types

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
 fn main() {
-    println!("Hai Kenedy Nopriansyah");
+    let t: (i32, f32, String, bool) = (32, 2.32, "Hahaha".to_string(), false);
+    let arr: [i32; 5] = [1, 2, 3, 4, 5];
+    let slice: &[i32] = &arr[1..3];
+
+    println!("{:?}\n{:?}\n{:?}", t, arr, slice);
 }
