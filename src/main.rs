a1041175cfd19d22cdd8026741f4789e8b1700c9
Kenedy Nopriansyah <kenedynopriansyah@192.168.1.18>
2025-02-26 14:55:09 +0700

    data types double

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 fn main() {
-    let t: (i32, f32, String, bool) = (32, 2.32, "Hahaha".to_string(), false);
     let arr: [i32; 5] = [1, 2, 3, 4, 5];
-    let slice: &[i32] = &arr[1..3];
+    let tuple: (i32, f32, bool, String) = (12, 1.12, false, "Kenedy Nopriansyah".to_string());
+    let slice: &[i32] = &arr[0..3];
 
-    println!("{:?}\n{:?}\n{:?}", t, arr, slice);
+    println!("{:?}", arr);
+    println!("{:?}", tuple);
+    println!("{:?}", slice);
 }
