1e9c1b792d4fed8cbe24f6897e6c45a93be13226
Kenedy Nopriansyah <kenedynopriansyah@192.168.1.18>
2025-02-27 16:27:27 +0700

    functions

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
 fn main() {
-    println!("Hai Kenedy Nopriansyah");
+    println!("{}", print_numbers_to(32));
+
+    arr_func(10);
+}
+
+fn print_numbers_to(value: i32) -> i32 {
+    return value;
+}
+
+fn arr_func(num: i32) {
+    println!("ini value dari arr func");
+    for i in 0..num {
+        println!("{}", i + 1);
+    }
 }
