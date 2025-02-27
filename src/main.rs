bb82f2ec6a5ba6ae3c95cfcda2980a84ed574322
Kenedy Nopriansyah <kenedynopriansyah@192.168.1.18>
2025-02-27 19:57:14 +0700

    shadowing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
-    println!("Hai Kenedy Nopriansyah");
+    let mut n = 10;
+
+    {
+        let n = 12;
+
+        println!("{}", n);
+    }
+
+    n = 100;
+
+    println!("{}", n);
 }
