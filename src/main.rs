7ff3bba228d8bbb3a28f84fa61b794a32521d82c
Kenedy Nopriansyah <kenedynopriansyah@192.168.1.18>
2025-02-28 17:30:04 +0700

    struct

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+struct Color {
+    red: i32,
+    blue: i32,
+    green: i32,
+}
+
 fn main() {
-    println!("Hai Kenedy Nopriansyah");
+    let mut color = Color {
+        red: 100,
+        blue: 200,
+        green: 300,
+    };
+
+    color.green = 50;
+
+    println!(
+        "red : {} blue : {} green : {}",
+        color.red, color.blue, color.green
+    );
 }
