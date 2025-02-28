d1bfdc729fb24500d5a078882dafa57bc6ed2713
Kenedy Nopriansyah <kenedynopriansyah@192.168.1.18>
2025-02-28 19:41:46 +0700

    pass by reference

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+struct Color {
+    red: i32,
+    blue: i32,
+    green: i32,
+}
+
 fn main() {
-    println!("Hai Kenedy Nopriansyah");
+    let color = Color {
+        red: 50,
+        blue: 80,
+        green: 1,
+    };
+
+    print_colors(&color);
+    print_colors(&color);
+}
+
+fn print_colors(color: &Color) {
+    println!(
+        "red {} blue {} green {}",
+        color.red, color.blue, color.green
+    );
 }
