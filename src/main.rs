8028b7cd70abb03f4ce4ac36f62985f1689727c7
Kenedy Nopriansyah <kenedynopriansyah@192.168.1.18>
2025-02-28 17:36:03 +0700

    tuples and struct

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+struct Color(i32, i32, i32);
+
 fn main() {
-    println!("Hai Kenedy Nopriansyah");
+    let mut green = Color(255, 0, 0);
+
+    green.2 = 255;
+
+    println!("R {} G {} B {}", green.0, green.1, green.2);
 }
