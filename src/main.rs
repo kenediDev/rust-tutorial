20541b74d6f34c9f3eff1cc403dd793fd5887b9a
Kenedy Nopriansyah <kenedynopriansyah@192.168.1.8>
2025-03-01 20:00:39 +0700

    strings

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
 fn main() {
-    println!("Hai Kenedy Nopriansyah");
+    let mut strings = String::from("Nama saya adalah Kenedy Nopriansyah");
+
+    println!("length {}", strings.len());
+
+    println!("is empty {}", strings.is_empty());
+
+    println!("ini adalah value dari split white space : ");
+
+    for i in strings.split_whitespace() {
+        println!("{}", i);
+    }
+
+    println!("contains {}", strings.contains("Kenedy"));
+
+    strings.push_str(" dan ini dari youtube Kenedy Nopriansyah");
+
+    println!("{}", strings);
 }
