e9dc2bcab38bd04c48d6dc992e601632bb52b2ca
Kenedy Nopriansyah <kenedynopriansyah@192.168.1.8>
2025-03-01 23:26:45 +0700

    defining traits

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+struct Person {
+    name: String,
+    age: u8,
+}
+
+trait TraitPerson {
+    fn print_to(&self);
+    fn is_square(&self) -> bool;
+}
+
+impl TraitPerson for Person {
+    fn print_to(&self) {
+        println!("Name {} Age {}", self.name, self.age);
+    }
+
+    fn is_square(&self) -> bool {
+        return self.age > 19;
+    }
+}
+
 fn main() {
-    println!("Hai Kenedy Nopriansyah");
+    let person = Person {
+        name: String::from("Kenedy Nopriansyah"),
+        age: 18,
+    };
+
+    person.print_to();
+
+    println!("{}", person.is_square());
 }
