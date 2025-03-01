e182e6fea78f39569c6d370569850fc8aa7f5a9e
Kenedy Nopriansyah <kenedynopriansyah@192.168.1.8>
2025-03-01 23:12:59 +0700

    writing to file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use std::fs::File;
+use std::io::prelude::*;
+
 fn main() {
-    println!("Hai Kenedy Nopriansyah");
+    let mut file = File::create("create_file.txt").expect("File tidak bisa dibuat");
+
+    file.write_all(b"ini dari youtube channel Kenedy Nopriansyah")
+        .expect("Maaf file tidak bisa dibuat");
+
+    let mut f = File::open("create_file.txt").expect("Maaf file tidak ditemukan");
+
+    let mut contents = String::new();
+
+    f.read_to_string(&mut contents)
+        .expect("Maaf file tidak bisa dibaca");
+
+    println!("{}", contents);
 }
