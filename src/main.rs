938767f30f49e71665017ae39e0ca925c78b6eef
Kenedy Nopriansyah <kenedynopriansyah@192.168.1.8>
2025-03-01 21:18:09 +0700

    implementing traits

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+struct Person {
+    name: String,
+    age: u8,
+}
+
+impl ToString for Person {
+    fn to_string(&self) -> String {
+        return format!("Name : {} Age : {}", self.name, self.age);
+    }
+}
+
 fn main() {
-    println!("Hai Kenedy Nopriansyah");
+    let person = Person {
+        name: String::from("Kenedy Nopriansyah"),
+        age: 18,
+    };
+
+    println!("{}", person.to_string());
 }
