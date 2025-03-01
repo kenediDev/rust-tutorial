508d6f12b477a6d93d9e83764d6dac1415864724
Kenedy Nopriansyah <kenedynopriansyah@192.168.1.8>
2025-03-01 22:11:23 +0700

    vector

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
 fn main() {
-    println!("Hai Kenedy Nopriansyah");
+    let mut vector = vec![100, 23, 55, 92, 52];
+
+    vector.remove(1);
+    vector.push(999);
+
+    for i in 0..vector.len() {
+        println!("index {}, value {}", i, vector[i]);
+    }
 }
