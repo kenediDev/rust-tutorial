ecacbab0beb7262c47ab5fef442499a74865f239
Kenedy Nopriansyah <kenedynopriansyah@192.168.1.8>
2025-03-01 19:41:34 +0700

    impl

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+struct Rectangle {
+    width: i32,
+    height: i32,
+}
+
+impl Rectangle {
+    fn print_description(&self) {
+        println!("width {} height {}", self.width, self.height);
+    }
+    fn is_square(&self) -> bool {
+        self.width != self.height
+    }
+}
+
 fn main() {
-    println!("Hai Kenedy Nopriansyah");
+    let rectangle = Rectangle {
+        width: 100,
+        height: 100,
+    };
+
+    rectangle.print_description();
+    println!("{}", rectangle.is_square());
 }
