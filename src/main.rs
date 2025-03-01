9b284ba3be1392a19832a1f345743e650eb263a8
Kenedy Nopriansyah <kenedynopriansyah@192.168.1.8>
2025-03-02 01:11:09 +0700

    hash maps

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use std::collections::HashMap;
+
 fn main() {
-    println!("Hai Kenedy Nopriansyah");
+    let mut marks = HashMap::new();
+
+    marks.insert("Rust Programming", 40);
+    marks.insert("Web Development", 90);
+    marks.insert("UX Design", 95);
+    marks.insert("Proffersional Computing Studies", 20);
+
+    println!("Length {}", marks.len());
+    println!("{:?}", marks);
+
+    marks.remove("UX Design");
+
+    println!("New HashMap : {:?}", marks);
+
+    marks.insert("New UX Design", 100);
+
+    println!("New HashMap Add UX Design : {:?}", marks);
+
+    for (key, value) in &marks {
+        println!("Key : {}, Value {}", key, value);
+    }
 }
