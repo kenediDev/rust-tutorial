52e00d6b1c3c642efcf10c798b614e2c6b81bf72
Kenedy Nopriansyah <kenedynopriansyah@192.168.1.8>
2025-03-01 22:29:01 +0700

    reading a file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::fs::File;
+use std::io::prelude::*;
+
 fn main() {
-    println!("Hai Kenedy Nopriansyah");
+    let mut file = File::open("nama_file.txt").expect("File tersebut tidak ada");
+
+    let mut contents = String::new();
+
+    file.read_to_string(&mut contents)
+        .expect("File tidak ditemukan");
+
+    println!("{}", contents);
 }
