8603059f5d89fec91938c722b127168140ca290e
Kenedy Nopriansyah <kenedynopriansyah@192.168.1.8>
2025-03-02 04:49:41 +0700

    regular expression

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use regex::Regex;
+
 fn main() {
-    println!("Hai Kenedy Nopriansyah");
+    let regex = Regex::new(r"\w{6}").unwrap();
+
+    match regex.captures("Kened") {
+        Some(c) => println!("{}", &c[0]),
+        None => println!("Regex tidak sesuai"),
+    }
 }
