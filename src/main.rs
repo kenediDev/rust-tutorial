c6a13cdc254afe405dd55cf0d8132655dbce60ab
Kenedy Nopriansyah <kenedynopriansyah@192.168.1.8>
2025-03-02 02:40:47 +0700

    random numbers

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use rand::prelude::*;
+
 fn main() {
-    println!("Hai Kenedy Nopriansyah");
+    let random = rand::rng().random_range(0..=10);
+    println!("{}", random);
+
+    let mut arr: [i32; 5] = [1, 2, 3, 4, 5];
+
+    arr.shuffle(&mut rand::rng());
+
+    println!("{:?}", arr);
 }
