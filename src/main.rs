e6742a66e99c3fb37c4b980cbdd174f4271bd20f
Kenedy Nopriansyah <kenedynopriansyah@Kenedys-MacBook-Air.local>
2025-03-02 15:56:52 +0700

    enum methods

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
 fn main() {
-    println!("Hai Kenedy Nopriansyah");
+    println!(
+        "Value {}",
+        match option("empty") {
+            Some(c) => c,
+            None => "Not have value",
+        }
+    )
+}
+
+fn option(value: &str) -> Option<&str> {
+    match value {
+        "Munich" => Some("This is munich"),
+        "spain" => Some("This is spain"),
+        _ => None,
+    }
 }
