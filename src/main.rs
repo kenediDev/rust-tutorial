24e7b98f5101e049cf68b254e7bbd42c1d09f6a3
Kenedy Nopriansyah <kenedynopriansyah@192.168.1.8>
2025-03-02 21:13:19 +0700

    parsing json

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+use serde_derive::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize)]
+struct Person {
+    fullName: String,
+    age: u8,
+    is_male: bool,
+}
+
 fn main() {
-    println!("Hai Kenedy Nopriansyah");
+    let data = r#"
+    {
+        "fullName": "Kenedy Nopriansyah",
+        "age": 18,
+        "is_male": true
+    }
+        "#;
+
+    let res = serde_json::from_str(data);
+    if res.is_ok() {
+        let result: Person = res.unwrap();
+        println!(
+            "Name : {}, Age : {}, Is Male : {}",
+            result.fullName, result.age, result.is_male
+        );
+    } else {
+        println!("Data tidak dapat diparse");
+    }
 }
